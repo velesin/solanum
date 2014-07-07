5ac72c296af9817d18d7e8229b1ec30b10cdfc3e
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2014-07-07 08:07:27 +0200

    small aoo.js clean up

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,15 @@
 'use strict'
 
+var gui = require('nw.gui');
 var nodeWebkit = require('./app/webkit_service');
 var clock = require('./app/clock_service');
-var timerCtrl = require('./app/timer_controller');
 var asClockFilter = require('./app/as_clock_filter');
+var timerController = require('./app/timer_controller');
 
 angular.module('timerApp', [])
   .value('nodeWebkit', nodeWebkit)
   .factory('clock', clock)
-  .controller('TimerCtrl', timerCtrl)
-  .filter('asClock', asClockFilter);
+  .filter('asClock', asClockFilter)
+  .controller('TimerController', timerController);
 
-nodeWebkit.start(require('nw.gui'));
+nodeWebkit.start(gui);
