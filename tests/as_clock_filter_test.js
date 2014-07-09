2494ac7a0630c4ba4cbc2b5dbf6afd6226cef339
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2014-07-09 08:30:28 +0200

    unit tests for asClock filter

--- tests/as_clock_filter_test.js
+++ tests/as_clock_filter_test.js
@@ -12,6 +12,18 @@ function timeInMsec(hours, minutes, seconds) {
 
 describe('asClockFilter', function() {
   it('formats time correctly for two digit h, m and s', function() {
-    expect(filter(timeInMsec(11, 22, 33))).to.equal('11:22:33');
+    expect(filter(timeInMsec(12, 34, 56))).to.equal('12:34:56');
+  });
+
+  it('formats time correctly for single digit h, m and s', function() {
+    expect(filter(timeInMsec(1, 2, 3))).to.equal('01:02:03');
+  });
+
+  it('formats time correctly for no h, m and s', function() {
+    expect(filter(0)).to.equal('00:00:00');
+  });
+
+  it('rounds the time down to a full second', function() {
+    expect(filter(secondInMsec + 123)).to.equal('00:00:01');
   });
 });
