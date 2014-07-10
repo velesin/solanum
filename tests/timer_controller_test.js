8abeecb1984506b2196ca2f6757eb040e9f38c84
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2014-07-10 08:31:46 +0200

    timer controller tests

--- tests/timer_controller_test.js
+++ tests/timer_controller_test.js
@@ -7,6 +7,16 @@ var nodeWebkit;
 var timerCallbacks;
 var timer;
 
+var itStartsBreak = function() {
+  it('starts the clock, counting up', function() {
+    expect(timer.up).to.have.been.called;
+  });
+
+  it('blocks the screen', function() {
+    expect(nodeWebkit.break).to.have.been.called;
+  });
+};
+
 describe('TimerController', function() {
   beforeEach(function() {
     $scope = {};
@@ -25,5 +35,79 @@ describe('TimerController', function() {
       timerCallbacks = callbacks;
       return timer;
     });
+  });
+
+  it('initially shows zeroed time', function() {
+    expect($scope.elapsedTime).to.equal(0);
+  });
+
+  describe('focus handler', function() {
+    beforeEach(function() {
+      $scope.focus();
+    });
+
+    it('starts the clock, counting down', function() {
+      expect(timer.down).to.have.been.called;
+    });
+
+    it('hides the app window', function() {
+      expect(nodeWebkit.focus).to.have.been.called;
+    });
+  });
+
+  describe('break handler', function() {
+    beforeEach(function() {
+      $scope.break();
+    });
+
+    itStartsBreak();
+  });
+
+  describe('stop handler', function() {
+    beforeEach(function() {
+      $scope.stop();
+    });
+
+    it('stops the clock', function() {
+      expect(timer.stop).to.have.been.called;
+    });
+
+    it('hides the app window', function() {
+      expect(nodeWebkit.focus).to.have.been.called;
+    });
+
+    it('zeroes the time', function() {
+      expect($scope.elapsedTime).to.equal(0);
+    });
+  });
+
+  describe('tick callback handler', function() {
+    var elapsedTime = 123;
+
+    beforeEach(function() {
+      timerCallbacks.tick(elapsedTime);
+    });
+
+    it('updates elapsed time', function() {
+      expect($scope.elapsedTime).to.equal(elapsedTime);
+    });
+  });
+
+  describe('break warning callback handler', function() {
+    beforeEach(function() {
+      timerCallbacks.warning();
+    });
+
+    it('displays incoming break warning window', function() {
+      expect(nodeWebkit.breakWarning).to.have.been.called;
+    });
+  });
+
+  describe('end callback handler', function() {
+    beforeEach(function() {
+      timerCallbacks.end();
+    });
+
+    itStartsBreak();
   });
 });
